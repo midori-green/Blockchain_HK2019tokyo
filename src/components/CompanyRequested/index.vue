39557137a195d427305655e67f86c63ae88d8092
haryu703 <34744512+haryu703@users.noreply.github.com>
2019-02-16 21:45:25 +0900

    merge student to company

--- src/components/CompanyRequested/index.vue
+++ src/components/CompanyRequested/index.vue
@@ -2,21 +2,26 @@
 <style src="./style.css" scoped></style>
 
 <script>
+let network = require("../../lib/network.js")
+
 export default {
   data() {
     return {
-      txid: '',
-      redeemScript: '',
+      studentEmail: this.$route.query.studentEmail,
+      replyEmail: this.$route.query.replyEmail,
+      txid: this.$route.query.txid,
+      redeemScript: this.$route.query.redeemScript,
+      subject: `${process.title}です。会社からのトランザクションに署名してください。`,
+      body: null,
+      email_link: null
     }
   },
   mounted() {
-    this.txid = this.$route.query.txid
-    this.redeemScript = this.$route.query.redeemScript
-    const studentMail = this.$route.query.studentMail
-    const replyMail = this.$route.query.replyMail
+    this.body = `redeemScript: ${this.redeemScript}, txinfo: ${network.explorer}${this.txid}, 返信先アドレス: ${this.replyEmail}
+以上の情報を検証し、正しければ以下のリンクから署名をしてください。
+http://localhost:8101/university/sign?redeemScript=${this.redeemScript}&txid=${this.txid}`
 
-    const link = ''
-    // TODO: send email
+    this.email_link = `mailto:${this.studentEmail}?subject=${this.subject}&amp;body=${this.body}`
   }
 }
 </script>
